50b8c5237d145376bbd9f25ab38ae58ed9679044
Olivier Valette <olivier.valette@spi10.com>
2018-12-10 17:05:02 +0100

    Nicer badge

--- src/Component/ItemStation.js
+++ src/Component/ItemStation.js
@@ -5,7 +5,7 @@ class ItemStation extends Component {
 
     render() {
         return (
-            <ListGroupItem className="d-flex justify-content-between">
+            <ListGroupItem className="d-flex justify-content-between align-items-center">
                 {this.props.station.nom}
                 <Badge bsStyle="primary" className="badge-pill">{this.props.station.nombrevelosdisponibles}</Badge>
                 </ListGroupItem>
